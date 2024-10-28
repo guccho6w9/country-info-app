c17f6b28870e3f0bb3ee4270f95576d279b1012f
guccho6w9 <gusta.quiroga033@gmail.com>
2024-10-28 19:13:00 -0300

    Add(-): updated services

--- BE/services/countryService.js
+++ BE/services/countryService.js
@@ -13,27 +13,27 @@ exports.fetchCountryInfo = async (countryCode) => {
   console.log(`Fetching country info for: ${countryCode}`);
 
   try {
-    // Obtener el código ISO3 usando REST Countries
+
     const restCountriesResponse = await axios.get(`${REST_COUNTRIES_API_URL}/alpha/${countryCode}`);
-    const iso3Code = restCountriesResponse.data[0]?.cca3 || ""; // ISO3 obtenido
+    const iso3Code = restCountriesResponse.data[0]?.cca3 || ""; 
 
     if (!iso3Code) {
       console.warn(`No se encontró el código ISO3 para el país con código ISO2: ${countryCode}`);
       return { message: "Country ISO3 code not found" };
     }
 
-    // Obtener información del país de Nager API
+
     const countryInfoResponse = await axios.get(`${NAGER_API_URL}/CountryInfo/${countryCode}`);
     const countryInfo = countryInfoResponse.data;
     const borders = countryInfo.borders || [];
 
-    // Obtener datos de población usando el código ISO3
+
     const populationResponse = await axios.get(`${COUNTRIES_NOW_API_URL}/countries/population`);
     const populationData = populationResponse.data.data;
     const countryPopulation = populationData.find(item => item.iso3 === iso3Code);
     const formattedPopulationData = countryPopulation ? countryPopulation.populationCounts : [];
 
-    // Obtener la bandera usando ISO2
+
     const flagsResponse = await axios.get(`${COUNTRIES_NOW_API_URL}/countries/flag/images`);
     const flagData = flagsResponse.data.data;
     const countryFlag = flagData.find(flag => flag.iso2 === countryCode) || {};
@@ -43,7 +43,7 @@ exports.fetchCountryInfo = async (countryCode) => {
       officialName: countryInfo.officialName || "No official name available",
       region: countryInfo.region || "No region available",
       borders,
-      iso3Code,  // Incluyendo ISO3 en la respuesta
+      iso3Code, 
       populationData: formattedPopulationData.length > 0 ? formattedPopulationData : "No population data available",
       flagUrl: countryFlag.flag || 'No flag available',
     };
